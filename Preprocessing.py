d494745df8ae8863fd5272ff99472f58a02a9069
Salman Mohammed <sm10793@gmail.com>
2018-08-18 18:52:07 -0700

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -15,15 +15,15 @@ for tline in data:
     print ('------' + tline)
     
    # tline=h_p.unescape(tline,"")
-  #  tline=h_p.decode("utf8").encode('ascii','ignore')
-  #  p.clean(tline)
+   # tline=h_p.decode("utf8").encode('ascii','ignore')
+   # p.clean(tline)
 
     tline = tline.replace('\n','')  
     tline = ' '.join(re.sub("RT|http\S+|(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+|)","",tline).split())
     tline = re.sub(r'\b\d+\b', '', tline)
     
    #1 tline = ' '.join(re.sub("RT|@|https:|t.co|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",tline).split())
-    #tline = ' '.join(re.sub("RT|@|http\S+|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",tline).split())
+   # tline = ' '.join(re.sub("RT|@|http\S+|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",tline).split())
    # tline = ''.join(re.sub(r'^https?:\/\/.*[\r\n]*', "", tline, flags=re.MULTILINE).split()) 
     print ('******'+tline)
     writeobj.write(tline+'\n')
